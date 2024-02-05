da3dd4260387147d1153c33ce58c3035a0a398aa
Edward Laird <52163138+EdwardLaird1@users.noreply.github.com>
2024-02-05 20:38:19 +0000

    Warn about files that are too large or contain too many comments.
    These slow down marking on an iPad. Better to optimise them first by hand.

--- Python/Meld.py
+++ Python/Meld.py
@@ -13,20 +13,25 @@ from pypdf import PdfMerger, PdfWriter, PdfReader
 from pypdf.annotations import FreeText
 
 
-#Declare constants
+# Declare constants
 
 # Folder path: the name of the folder you want to meld
-dir_path = r'C:\Users\lairde\OneDrive - Lancaster University\OneDrive Documents\Teaching\Lecturing\PHYS102\2022\Laird'
-#Key file: CSV containing information about the files that were melded
-KeyFileName = 'keyfile.csv'
-#Melded fild name: Name of melded file that you want to create
-MeldedFileName = 'MeldedPDF.pdf'
-#Path to melded file name
-melded_file_path = dir_path + '\\' + MeldedFileName
-#Expected number of files per directory. Usually this is 1 or 2, but some students upload many files.
+dir_path = r'C:\Users\lairde\OneDrive - Lancaster University\OneDrive Documents\Teaching\Lecturing\PHYS102\2023-24\Exam\Q12'
+# Key file: CSV containing information about the files that were melded
+key_file_name = 'keyfile.csv'
+# Melded fild name: Name of melded file that you want to create
+melded_file_name = 'MeldedPDF.pdf'
+# Path to melded file name
+melded_file_path = dir_path + '\\' + melded_file_name
+# Expected number of files per directory. Usually this is 1 or 2, but some students upload many files.
 expectedNumFiles=range (1,10)
-#Whether to print the student name at the top of their work, or just their ID
-PrintNameOnScript = True
+# Whether to print the student name at the top of their work, or just their ID
+Print_name_on_script = False
+# Size of file (in MB) above which to print a warning
+warning_file_size = 10
+# Number of annotations on a page above which to print a warning
+warning_annotation_number = 5
+
 
 def numItems(root_path, path):
     """
@@ -62,56 +67,71 @@ listOfDirectories = list(filter(lambda item: os.path.isdir(dir_path+'\\'+item), 
 # Remove everything that contains the wrong number of files
 listOfDirectories = list(filter(lambda item: 1 <= numItems(dir_path, item) <= 2, listOfDirectories))
 
-with open(dir_path + '\\' + KeyFileName, 'w', newline='') as keyfile:
-    writer = csv.writer(keyfile)
+with open(dir_path + '\\' + key_file_name, 'w', newline='') as keyfile:
+    key = csv.writer(keyfile)
     
-    #Create anobject to merge the PDF files into
-    merger = PdfMerger()
+    #Create an object to merge the PDF files into
+    unscaled_merged_pdf = PdfMerger()
     
     #Iterate through the student directories, merging pdf files together and recording their names and lengths in keyfile.csv
     for item in listOfDirectories:
         for file in os.listdir(dir_path + '\\' + item):
-            readpdf = pypdf.PdfReader(dir_path + '\\' + item + '\\' + file)
-            totalPages = len(readpdf.pages)
-            line = [item, file, totalPages]
-            writer.writerow(line)
-            merger.append(dir_path + '\\' + item + '\\' + file)
-    merger.write(dir_path + '\\' +'MeldedPDF_unscaled.pdf')
-    merger.close()
+            if file.endswith(".pdf"):    #Merge only pdfs. Ignore other files.
+                #Warn if file too big:
+                file_stats = os.stat(dir_path + '\\' + item + '\\' + file)
+                file_size = file_stats.st_size / (1024 * 1024)
+                if (file_size > warning_file_size):
+                    print('Warning: The following file is uncommonly large (', round(file_size,1),'MB). You may want to optimise it before marking:\n', item, file)
+                readpdf = pypdf.PdfReader(dir_path + '\\' + item + '\\' + file)
+                #Warn if too many annotations (This happens when students write their work on an iPad, and makes the melded file unwieldy.)
+                for page in readpdf.pages:
+                    if "/Annots" in page:
+                        if (len(page["/Annots"]) > warning_annotation_number):
+                            print('Warning: The following file contains a large number of annotations (', len(page["/Annots"]),'). You may want to print it to an image before marking:\n', item, file)
+                #Merge pdfs and record their details in the key file
+                totalPages = len(readpdf.pages)
+                line = [item, file, totalPages]
+                key.writerow(line)
+                unscaled_merged_pdf.append(dir_path + '\\' + item + '\\' + file)
+    unscaled_merged_pdf.write(dir_path + '\\' +'MeldedPDF_unscaled.pdf')
+    unscaled_merged_pdf.close()
     
     #Scale the pages in MeldedPDF.pdf to all have A4 width
     reader = PdfReader(dir_path + '\\' +'MeldedPDF_unscaled.pdf')
     writer = PdfWriter()
     for x in range(len(reader.pages)):
-        # add a page  from reader, but scale it to A4 (i.e. 595 points):
+        # Add a page  from reader, but scale it to A4 (i.e. 595 points):
         page = reader.pages[x]
         page.scale_to(595, 595 * float(page.mediabox.top) / float(page.mediabox.right))
         writer.add_page(page)
-        print('Processing page ', x+1, ' of ', len(reader.pages))
+        print('Processing page', x+1, 'of', len(reader.pages))
     os.remove(dir_path + '\\' +'MeldedPDF_unscaled.pdf')
     
     #Iterate again through the student directories, this time adding labels to the merged PDF near the top of each appropriate page.
     PageCounter = 0
     for item in listOfDirectories:
         for file in os.listdir(dir_path + '\\' + item):
-            readpdf = pypdf.PdfReader(dir_path + '\\' + item + '\\' + file)
-            page = reader.pages[PageCounter]
-            
-            #Create the label
-            StudentName = item.split('_')[0]
-            StudentID = item.split('_')[1]
-            ScriptLabel = StudentName + ' ' + StudentID if PrintNameOnScript else StudentID
-            
-            #Add the label as an annotation
-            annotation = FreeText(
-                text=ScriptLabel,
-                rect=(5,  float(page.mediabox.top)-20,150,  float(page.mediabox.top)-5),
-                font_size="8pt",
-                font_color="ff0000",
-                border_color="ffffff",
-                )
-            writer.add_annotation(page_number=PageCounter, annotation=annotation)
-            PageCounter += len(readpdf.pages)
+            if file.endswith(".pdf"):
+                readpdf = pypdf.PdfReader(dir_path + '\\' + item + '\\' + file)
+                page = reader.pages[PageCounter]
+                
+                #Create the label. Lancaster Moodle seems to use two different directory naming conventions, so comment out the wrong one.
+                #StudentName = item.split('_')[0]       #Coursework Moodle
+                #StudentID = item.split('_')[1]
+                StudentName = item.split(' - ')[1]      #Exam Moodle
+                StudentID = item.split(' - ')[0]
+                ScriptLabel = StudentName + ' ' + StudentID if Print_name_on_script else StudentID
+                
+                #Add the label as an annotation
+                annotation = FreeText(
+                    text=ScriptLabel,
+                    rect=(5,  float(page.mediabox.top)-20,150,  float(page.mediabox.top)-5),
+                    font_size="8pt",
+                    font_color="ff0000",
+                    border_color="ff0000",
+                    )
+                writer.add_annotation(page_number=PageCounter, annotation=annotation)
+                PageCounter += len(readpdf.pages)
             
     #Write the scaled annotated PDF as our final output.
     with open(melded_file_path, "xb") as fp:
